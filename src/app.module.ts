e9c3f2bb70b7e405e3cb03e742f61d50e3ceb4ca
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-02-05 23:10:37 +0530

    feat(configuration): Implement nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.production.local', '.env'],
+      load: [configuration],
+    }),
+  ],
 })
 export class AppModule {}
